ed923b06ea434933f2ad0f9cb941b860b87cc853
Alexander <aleksandr_chori@mail.ru>
2016-11-16 03:33:22 +1000

    Update converter_BBCode_HTML.pl
    Release version

--- converter_BBCode_HTML.pl
+++ converter_BBCode_HTML.pl
@@ -7,26 +7,48 @@ local $/;
 $_ = <$fin>;
 close $fin;
 
-my %simpleTranslate = ( 'b' => 'strong', 'i' => 'em', 's' => 'del' );
+my %simpleTranslate = ( 'b' => ['strong', ''], 'i' => ['em', ''], 's' => ['del', ''],
+	'u' => ['span', ' style="text-decoration: underline;"'], 'ul' => ['ul', ''], 'li' => ['li', ''],
+	'ol' => ['ol', ''], 'table' => ['table', ' style="border: 1px solid;"'], 'tr' => ['tr', ''],
+	'td' => ['td', ''], 'quote(?:=\"[^\]]*\")?' => ['blockquote><p', ''],
+	'code' => ['code', ' style="white-space: pre;"'] );
 
 while (my ($BBTag, $HTMLTag) = each %simpleTranslate) {
-	while (s/\[$BBTag\]((?:(?!\[$BBTag\]|\[\/$BBTag\]).)*)\[\/$BBTag\]/
-		"<$HTMLTag>".$1."<\/$HTMLTag>"/ge) {}
+	while(s/\[$BBTag\]((?:(?!\[$BBTag\]|\[\/$BBTag\]).)*)\[\/$BBTag\]/
+		"<@{$HTMLTag}[0]@{$HTMLTag}[1]>".$1."<\/@{$HTMLTag}[0]>"/gse) {}
 }
 
-my %translateParameter = ( 'u' => ['span style="text-decoration: line-through;"', 'span'],
-						   'code' => ['code style="white-space: pre;"', 'code'] );
+while(s/\[img\]((?:(?!\[img\]|\[\/img\]).)*)\[\/img\]/
+	"<img src=\"$1\" \/>"/gse) {}
 
-while (my ($BBTag, @HTMLTag) = each %translateParameter) {
-	while (s/\[$BBTag\]((?:(?!\[$BBTag\]|\[\/$BBTag\]).)*)\[\/$BBTag\]/
-		"<$HTMLTag[0][0]>".$1."<\/$HTMLTag[0][1]>"/ge) {}
+while(s/\[url\]((?:(?!\[url\]|\[\/url\]).)*)\[\/url\]/
+		"<a href=\"$1\">".$1.'<\/a>'/gse) {}
+while (s/\[url=([^\]]*)\]((?:(?!\[url\]|\[\/url\]).)*)\[\/url\]/
+	"<a href=\"$1\">".$2.'<\/a>'/gse) {}
+
+my %capturingTranslate = ( 'size' => 'font-size: ', 'color' => 'color: ' );
+
+while (my ($BBTag, $CSSOption) = each %capturingTranslate) {
+	while (s/\[$BBTag=\"([^\"]*)\"\]((?:(?!\[$BBTag\]|\[\/$BBTag\]).)*)\[\/$BBTag\]/
+		"<span style=\"$CSSOption$1\;\">".$2.'<\/span>'/gse) {}
+	while (s/\[$BBTag=([^\]]*)\]((?:(?!\[$BBTag\]|\[\/$BBTag\]).)*)\[\/$BBTag\]/
+		"<span style=\"$CSSOption$1\;\">".$2.'<\/span>'/gse) {}
 }
-=pod
-my %translateParameterInside = (  );
 
-while (s/\[$BBTag=(.*)\]((?:(?!\[$BBTag\]|\[\/$BBTag\]).)*)\[\/$BBTag\]/
-		"<$HTMLTag[0][0]>".$2."<\/$HTMLTag[0][1]>"/ge) {}
-=cut
-open my $fout, '>', 'output.html';
+while (m/\[list\]((?:(?!\[list\]|\[\/list\]).)*)\[\/list\]/s) {
+	my $list = $1 =~ s/\[\*\]/<li>/r;
+	while ($list =~ s/\[\*\]/<\/li><li>/) {}
+	$list =~ s/(?=\z)/<\/li>/;
+	s/\[list\]((?:(?!\[list\]|\[\/list\]).)*)\[\/list\]/<ul>$list<\/ul>/s
+}
+
+
+while(m/\[style ([^\]]*)\]((?:(?!\[style\]|\[\/style\]).)*)\[\/style\]/) {
+	my $style = $1 =~ s/\=/\:/gr;
+	s/\[style ([^\]]*)\]((?:(?!\[style\]|\[\/style\]).)*)\[\/style\]/
+	"<span style=\"$style\;\">".$2.'<\/span>'/gse
+}
+
+open my $fout, ">", "output.html";
 say $fout $_;
 close $fout;
